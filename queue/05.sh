f14c5d96ff9c8411a5b9e31ed44c091465e01063
jibon <atikurrahaman386@gmail.com>
2023-04-08 15:41:32 +0600

    update

--- queue/05.sh
+++ queue/05.sh
@@ -1,2 +1,2 @@
 cd /home/jibon/python/autometic-deploy
-python scheduler.py --dev "2023-04-09 05:12 AM" -b "series-watch-info" -u "https://www.facebook.com/messages/t/5020246434759915/" -m "series <cl>e <cl>watch info add <cl>kore diyechi." -p "Profile 2" -wd "~/website/mymoviebazar.net" -wb "main"
+python scheduler.py -d "2023-04-09 05:12 AM" -b "series-watch-info" -u "https://www.facebook.com/messages/t/5020246434759915/" -m "series <cl>e <cl>watch info add <cl>kore diyechi." -p "Profile 2" -wd "~/website/mymoviebazar.net" -wb "main"
