b1bef337dada94227dcc4838aeae31a921a41d21
jibon <atikurrahaman386@gmail.com>
2023-04-09 14:29:21 +0600

    update

---
+++ queue/01.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/jibon/python/autometic-deploy
+
+python scheduler.py -d "2023-04-09 03:12 PM" -b "fix-pagination" -u "https://www.facebook.com/messages/t/5020246434759915" -m "<cl>pyajineSone kichu somosya cOkhe porechilO, Thik kore diyechi, ekhon heDar niye kaj korotechi." -p "Profile 2" -wd "~/website/mymoviebazar.net" -wb "main"
--- queue/queue.sh
+++ queue/queue.sh
@@ -1,11 +1,6 @@
+#!/bin/bash
 cd /home/jibon/python/autometic-deploy/queue
 
 ./01.sh && mv 01.sh _01.sh
 
 ./02.sh && mv 02.sh _02.sh
-
-./03.sh && mv 03.sh _03.sh
-
-./04.sh && mv 04.sh _04.sh
-
-./05.sh && mv 05.sh _05.sh
