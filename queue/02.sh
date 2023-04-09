86cca67cf5503b938106fb6fc33444f621c7fee5
jibon <atikurrahaman386@gmail.com>
2023-04-09 16:39:11 +0600

    update

--- queue/02.sh
+++ queue/02.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/jibon/python/autometic-deploy
 
-python scheduler.py -d "2023-04-10 02:55 AM" -b "fb-chat" -u "https://www.facebook.com/messages/t/5020246434759915" -m "fb messanger add <cl>kore diyechi." -p "Profile 2" -wd "~/website/mymoviebazar.net" -wb "main"
+python scheduler.py -d "2023-04-10 05:55 AM" -b "fb-chat" -u "https://www.facebook.com/messages/t/5020246434759915" -m "fb messanger add <cl>kore diyechi." -p "Profile 2" -wd "~/website/mymoviebazar.net" -wb "main"
