bac20422acfa2d1a15aee1b90a54363b02d200cd
jibon <atikurrahaman386@gmail.com>
2023-04-11 04:12:07 +0600

    update

--- queue/02.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/jibon/python/autometic-deploy/
-
-python scheduler.py -d "2023-04-11 03:12 AM" -b "desktop-fb-chat" -u "https://www.facebook.com/messages/t/5020246434759915" -m "<cl>DeskoTop varrSone fesobuk cyaT eD kore diyechi." -p "Profile 2" -wd "~/website/mymoviebazar.net" -wb "main"
