a5d45a9aa7852736791a8f779b4470f891d72a35
jibon <atikurrahaman386@gmail.com>
2023-04-12 04:41:02 +0600

    update

--- queue/02.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/jibon/python/autometic-deploy/
-
-python scheduler.py -d "2023-04-12 03:28 AM" -b "search-by-last-transaction-id" -u "https://www.facebook.com/messages/t/5020246434759915" -m "<cl>ekhon lasT TransekoSon aiDi diye sarrc dite paroben." -p "Profile 2" -wd "~/website/mymoviebazar.net/" -wb "main"
