268420105ada1cbe8f2139deeb839ffb8694fe37
jibon <atikurrahaman386@gmail.com>
2023-04-13 16:31:08 +0600

    update

---
+++ queue/01.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/jibon/python/autometic-deploy
+
+python scheduler.py -d "2023-04-14 06:03 AM" -b "recalculate-mac-reseller-bill" -u "https://www.facebook.com/messages/t/5020246434759915" -m "<cl>ekhon theke myak riselare protimase bil rikyalokuleT hobe." -p "Profile 2" -wd "~/website/media-online-billing" -wb "main"
