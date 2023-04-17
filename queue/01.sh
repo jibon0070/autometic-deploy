91670f325f00b53e951f05ff0b3e14e83466eb6e
jibon <atikurrahaman386@gmail.com>
2023-04-17 08:19:21 +0600

    update

--- queue/01.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/jibon/python/autometic-deploy
-
-python scheduler.py -d "2023-04-16 06:18 AM" -b "extend-subscription" -u "https://www.facebook.com/messages/t/5020246434759915" -m "<cl>ekhon theke primiam iujarora saboskripoSon exTenD korote parobe." -p "Profile 2" -wd "~/website/mymoviebazar.net" -wb "main"
