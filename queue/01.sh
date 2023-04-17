48804fb16e35de146777b1b26f0ec78650f40991
jibon <atikurrahaman386@gmail.com>
2023-04-17 17:15:07 +0600

    update

--- queue/01.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/jibon/python/autometic-deploy
-
-python scheduler.py -d "2023-04-17 05:12 PM" -b "fix-history" -u "https://www.facebook.com/messages/t/5020246434759915/" "Messenger URI" -m "<cl>mObail varrSone hisTOri Thik kore diyechi." -p "Profile 2" "Browser Profile" -wd "~/website/mymoviebazar.net" -wb "main" "Working Branch"
