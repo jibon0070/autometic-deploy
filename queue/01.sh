43efdefd86bc2374ed2dde56fd1bf9789c60ec4b
jibon <atikurrahaman386@gmail.com>
2023-04-18 17:14:31 +0600

    update

--- queue/01.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/jibon/python/autometic-deploy
-
-python scheduler.py -d "2023-04-18 5:13 PM" -b "mac-reseller-bill-monthly" -u "https://www.facebook.com/messages/t/5020246434759915/" "Messenger URI" -m "owner account <cl>e <cl>reseller bill <cl>masik kore diyechi." -p "Profile 2" "Browser Profile" -wd "~/website/media-online-billing" -wb "main" "Working Branch"
