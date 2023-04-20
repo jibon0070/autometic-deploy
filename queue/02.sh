4756b6fedb1dca034f0c5755c090ff049df38129
jibon <atikurrahaman386@gmail.com>
2023-04-20 15:23:34 +0600

    update

---
+++ queue/02.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/jibon/python/autometic-deploy
+
+python scheduler.py -d "2023-04-21 03:18 AM" -b "add-client-ip-in-users" -u "https://www.facebook.com/messages/t/5020246434759915/" "Messenger URI" -m "<cl>ekhon theke <cl>Users <cl>e byabohrrit SeSh <cl>ip address show <cl>korobe." -p "Profile 2" "Browser Profile" -wd "~/website/mymoviebazar.net" -wb "main" "Working Branch"
