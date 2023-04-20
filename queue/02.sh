3c49b1d8b20b3b7ab9c46a6306f7914fad9675a8
jibon <atikurrahaman386@gmail.com>
2023-04-20 14:19:39 +0600

    update

--- queue/02.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/jibon/python/autometic-deploy
-
-python scheduler.py -d "2023-04-20 02:18 PM" -b "fix-mac-reseller-due-bill" -u "https://www.facebook.com/messages/t/5020246434759915" -m "mac reseller <cl>e <cl>bill counting issue fix<cl>kore diyechi." -p "Profile 2" -wd "~/website/media-online-billing" -wb "main"
