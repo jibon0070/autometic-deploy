b985449cf4a054701b987f783bd38f07345452d8
jibon <atikurrahaman386@gmail.com>
2023-04-20 06:19:28 +0600

    update

--- queue/01.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/jibon/python/autometic-deploy
-
-python scheduler.py -d "2023-04-20 06:18 AM" -b "mac-reseller-collection-details" -u "https://www.facebook.com/messages/t/5020246434759915" -m "mac reseller <cl>e <cl>collection details add <cl>kore diyechi." -p "Profile 2" -wd "~/website/media-online-billing" -wb "main"
