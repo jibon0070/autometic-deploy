05587aac5b92890721705bf7c4e827e217f52489
jibon <atikurrahaman386@gmail.com>
2023-04-30 10:48:54 +0600

    update

---
+++ queue/02.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/jibon/python/autometic-deploy
+
+python scheduler.py --dev "2023-04-30 08:17 PM" -b "fix-user-ip-mobile-version" -u "https://www.facebook.com/messages/t/5020246434759915/" "Messenger URI" -m "Mobile version <cl>er iujar aipi fix kore diyechi." -p "Profile 2" "Browser Profile" -wd "~/website/mymoviebazar.net" -wb "main" "Working Branch"
