8c0d97b83bfd5f317b622f9ed2bd991bda02d3b8
jibon <atikurrahaman386@gmail.com>
2023-05-01 14:00:12 +0600

    update

---
+++ queue/01.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/jibon/python/autometic-deploy
+
+python scheduler.py -d "2023-05-01 07:17 PM" -b "search-user-by-ip" -u "https://www.facebook.com/messages/t/5020246434759915/" "Messenger URI" -m "<cl>ekhon theke aipi diye sarrc dile iujar cole asobe." -p "Profile 2" "Browser Profile" -wd "~/website/mymoviebazar.net" -wb "main" "Working Branch"
