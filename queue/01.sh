4669118c6cb5aafc29e1c96948c2034be223d43b
jibon <atikurrahaman386@gmail.com>
2023-05-04 15:42:46 +0600

    update

---
+++ queue/01.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/jibon/python/autometic-deploy
+
+python scheduler.py -d "2023-05-04 04:28 PM" -b "fix-bill" -u "https://www.facebook.com/messages/t/5020246434759915/" "Messenger URI" -m "billing <cl>er <cl>mac reseller bill counting issue <cl>Thik kore diyechi." -p "Profile 2" "Browser Profile" -wd "~/website/mymoviebazar.net" -wb "main" "Working Branch"
