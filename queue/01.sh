0c71ced5611f32c3d8e2e54fc9567cc5566bbf72
jibon <atikurrahaman386@gmail.com>
2023-05-24 20:00:50 +0600

    update

--- queue/01.sh
+++ queue/01.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/jibon/python/autometic-deploy
 
-python scheduler.py -d "2023-05-24 07:58 PM" -b "fix-users-search" -u "https://www.facebook.com/messages/t/5020246434759915/" "Messenger URI" -m "user search <cl>Thik kore diyechi." -p "Profile 2" "Browser Profile" -wd "~/website/media-online-billing" -wb "main" "Working Branch"
+python scheduler.py -d "2023-05-24 07:58 PM" -b "fix-users-search" -u "https://www.facebook.com/messages/t/5020246434759915/" "Messenger URI" -m "user search <cl>Thik kore diyechi." -p "Profile 2" "Browser Profile" -wd "~/website/mymoviebazar.net" -wb "main" "Working Branch"
